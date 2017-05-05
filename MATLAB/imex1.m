ed1f61cb1381f21889d40054fc4cfa42823e87e5
logemann <logemann@iastate.edu>
2017-05-05 13:14:23 -0500

    Finalized Course

--- MATLAB/imex1.m
+++ MATLAB/imex1.m
@@ -8,9 +8,9 @@ function [q] = imex1(q, A, H, N, Nt, Nx, ht, hx, epsilon)
         Hpn = cell2mat(arrayfun(@(i) H(q(1:N+1,i,n)), 1:Nx, 'UniformOutput', false));
 
         q1 = [q(1:N+1,:,n); (epsilon*q(N+2:end,:,n) + ht*Hpn)/(epsilon + ht)];
-        q1x = [q1(:,2:end), q1(:,end)] - q1(:,1:end);
-        q1xPlus = q1(:,1:end) - [q1(:,1), q1(:,1:end-1)];
-        q1xMinus = [q1(:,2:end), q1(:,end)] - q1(:,1:end);
+        q1x = [q1(:,2:end), q1(:,1)] - q1(:,1:end);
+        q1xPlus = q1(:,1:end) - [q1(:,end), q1(:,1:end-1)];
+        q1xMinus = [q1(:,2:end), q1(:,1)] - q1(:,1:end);
         q(:,:,n+1) = q(:, :, n) - (ht/hx) * (Aplus * q1xPlus + Aminus * q1xMinus) + ht/epsilon * [zeros(N+1, Nx); Hpn - q1(N+2:end,:)];
         %q(:,:,n+1) = q(:, :, n) + (ht/hx) * A * q1x + ht/epsilon * [zeros(N+1, Nx); Hpn - q1(N+2:end,:)];
     end
